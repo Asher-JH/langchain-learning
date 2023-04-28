f88e7749e491cad59964d53875584c630d03e83d
asher <asher@whiteroom.work>
2023-04-28 17:44:31 +0800

    simple-chain example

---
+++ simple-chain.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { OpenAI } from "langchain/llms/openai";
+
+export const run = async () => {
+  // temp controls how random/creative the res is. It ranges from 0 <-> 1
+  const model = new OpenAI({ temperature: 0.1 });
+
+  const res = await model.call("What is the national dish of Malaysia?");
+  console.log({ res });
+};
+
+run();
